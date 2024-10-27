c6d89202bc663c519024c5cc4149f3877bda01d2
GavrilS <gavr1lll@abv.bg>
2024-10-27 18:19:45 +0200

    Install script for prometheus on ubuntu and basic config file and systemd service file configuration

---
+++ prometheus_setup/prometheus_install_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install steps for Ubuntu 22.04
+# Update system packages with the sudo command
+apt update
+
+# Create a system user for prometheus - with the sudo command
+groupadd --system prometheus
+useradd -s /sbin/nologin --system -g prometheus prometheus
+
+# Create directories for prometheus - with the sudo command
+mkdir /etc/prometheus
+mkdir /var/lib/prometheus
+
+# Download prometheus and extract files
+wget https://github.com/prometheus/prometheus/releases/download/v2.55.0/prometheus-2.55.0.linux-amd64.tar.gz
+
+tar vxf prometheus*.tar.gz
+
+cd prometheus*/
+
+# Move the binary files and set owner - with sudo
+mv prometheus /usr/local/bin
+mv promtool /usr/local/bin
+chown prometheus:prometheus /usr/local/bin/prometheus
+chown prometheus:prometheus /usr/local/bin/promtool
+
+# Move the configuration files and set owner - with sudo
+mv consoles /etc/prometheus
+mv console_libraries /etc/prometheus
+mv prometheus.yml /etc/prometheus
+chown prometheus:prometheus /etc/prometheus
+chown -R prometheus:prometheus /etc/prometheus/consoles
+chown -R prometheus:prometheus /etc/prometheus/console_libraries
+chown -R prometheus:prometheus /var/lib/prometheus
+
+# Copy the base prometheus config content to the actual config file - with sudo
+mv prometheus.yml /etc/prometheus/prometheus.yml
+
+# Create prometheus systemd service - with sudo
+mv prometheus.service /etc/systemd/system/prometheus.service
