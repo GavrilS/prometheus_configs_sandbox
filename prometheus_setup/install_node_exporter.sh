0754488f3c75aa4d80d15c55b30d3531d9a2a0ef
GavrilS <gavr1lll@abv.bg>
2024-10-31 22:53:26 +0200

    Added script to install node_exporter

---
+++ prometheus_setup/install_node_exporter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
+tar xvfz node_exporter-*.*-amd64.tar.gz
+mv node_exporter-*.*-amd64 /etc/node_exporter-*.*-amd64
+rm node_exporter-*.*-amd64.tar.gz
+cd /etc/node_exporter-*.*-amd64
+./node_exporter
