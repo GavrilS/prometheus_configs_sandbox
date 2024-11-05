6d894300162bddd0bfa2a991894c7b052811bf9a
GavrilS <gavr1lll@abv.bg>
2024-11-05 21:43:08 +0200

    Updated the start_grafana script to include the option to decide which init system to use to start grafana based on script paramethers

--- grafana_setup/start_grafana.sh
+++ grafana_setup/start_grafana.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-# Start the grafana server with systemd
-sudo systemctl daemon-reload
-sudo systemctl start grafana-server
-sudo systemctl status grafana-server
+if [ "$#" -eq 0 ] || [ $1 = "systemd" ]
+then
+    # Start the grafana server with systemd
+    sudo systemctl daemon-reload
+    sudo systemctl start grafana-server
+    sudo systemctl status grafana-server
 
-# Configure the grafana server to start at boot using systemd
-sudo systemctl enable grafana-server.service
+    # Configure the grafana server to start at boot using systemd
+    sudo systemctl enable grafana-server.service
 
-# Edit the config file
-# Alternatively, create a file in /etc/systemd/system/grafana-server.service.d/override.conf
-# sudo systemctl edit grafana-server.service
+    # Edit the config file
+    # Alternatively, create a file in /etc/systemd/system/grafana-server.service.d/override.conf
+    # sudo systemctl edit grafana-server.service
 
-# Restart the grafana server using systemd
-# sudo systemctl restart grafana-server
-
-# Start the Grafana server using init.d
-# - start the server:
-# sudo service grafana-server start
-# sudo service grafana-server status
-# - configure grafana server to start at boot time with init.d:
-# sudo update-rc.d grafana-server defaults
-# sudo service grafana-server restart
+    # Restart the grafana server using systemd
+    # sudo systemctl restart grafana-server
+elif [ $1 = "init.d" ]
+then
+    # Start the Grafana server using init.d
+    # - start the server:
+    sudo service grafana-server start
+    sudo service grafana-server status
+    # - configure grafana server to start at boot time with init.d:
+    sudo update-rc.d grafana-server defaults
+    sudo service grafana-server restart
+else
+    echo "Invalid parameter specified for the 'start_grafana.sh' script!"
+fi
