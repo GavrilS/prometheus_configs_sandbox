a3a4c31c1dbd9ac174fb552ec4ed88d320b86b46
GavrilS <gavr1lll@abv.bg>
2025-02-02 17:12:48 +0200

    Added a script to query prometheus metrics with python and the dependency was added to the requirements file

--- tutorial/custom_metrics/get_deployment_max_replicas.py
+++ tutorial/custom_metrics/get_deployment_max_replicas.py
@@ -3,12 +3,18 @@ A script to first get the currently available deployment metrics from prometheus
 extract the max replicas label value into a separate metric.
 """
 from datetime import datetime
-from prometheus_api_client import PrometheusHost
-prom = PrometheusHost('localhost:9090', disable_ssl=True)
+from prometheus_api_client import PrometheusConnect
+prom = PrometheusConnect(url='http://localhost:9090', disable_ssl=True)
 
 
 def main():
-    pass
+    results = prom.custom_query(query="deployment_replicas")
+
+    # print('Printing results in the format: metric \\n metric_name, metric_labels', '\n*******\n')
+    for item in results:
+        print(item)
+        # print(item.metric_name, item.label_config, '\n*******\n')
+        
 
 
 if __name__=='__main__':
