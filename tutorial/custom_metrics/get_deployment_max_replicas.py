b70bf800646b3945253e6cc452e7c3427ef40aa9
GavrilS <gavr1lll@abv.bg>
2025-02-06 22:12:25 +0200

    Added functionality to extract a label value from a metric, create a new metric matching the label value and push it to Prometheus through Pushgateway

--- tutorial/custom_metrics/get_deployment_max_replicas.py
+++ tutorial/custom_metrics/get_deployment_max_replicas.py
@@ -3,18 +3,35 @@ A script to first get the currently available deployment metrics from prometheus
 extract the max replicas label value into a separate metric.
 """
 from datetime import datetime
+from prometheus_client import Gauge, CollectorRegistry, push_to_gateway
 from prometheus_api_client import PrometheusConnect
 prom = PrometheusConnect(url='http://localhost:9090', disable_ssl=True)
 
+registry = CollectorRegistry()
+DEPLOYMENT_MAX_REPLICAS = Gauge(
+    'deployment_max_replicas', 
+    'Deployment max replicas', 
+    ['deployment', 'namespace', 'job'],
+    registry=registry
+)
+
+PUSHGATEWAY_ENDPOINT = 'localhost:9091'
 
 def main():
     results = prom.custom_query(query="deployment_replicas")
 
-    # print('Printing results in the format: metric \\n metric_name, metric_labels', '\n*******\n')
+    print('Printing results for deployment_replicas', '\n*******\n')
     for item in results:
         print(item)
-        # print(item.metric_name, item.label_config, '\n*******\n')
-        
+        metric_labels = item.get('metric', {})
+        deployment = metric_labels.get('deployment', '')
+        max_replicas = int(metric_labels.get('max_replicas', 0))
+        namespace = metric_labels.get('namespace', '')
+        job = metric_labels.get('job', '')
+
+        DEPLOYMENT_MAX_REPLICAS.labels(deployment=deployment, namespace=namespace, job=job).set(max_replicas)
+        # DEPLOYMENT_MAX_REPLICAS.set_to_current_time()
+        push_to_gateway(PUSHGATEWAY_ENDPOINT, job=job, registry=registry)
 
 
 if __name__=='__main__':
